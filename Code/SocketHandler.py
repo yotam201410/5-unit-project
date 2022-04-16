97cc58cbdb941f608d1f7e21f286881b1b2bf1e7
yotam201410 <yotam201410@gmail.com>
2022-04-16 16:47:00 +0300

    still hav no idea wtf is going on

--- Code/SocketHandler.py
+++ Code/SocketHandler.py
@@ -1,5 +1,7 @@
 import logging
 import socket
+
+from Code.NetworkTalk.Computer import Computer
 from Code.NetworkTalk.MultiSocket import MultiSocket
 from typing import Tuple
 import threading
@@ -8,10 +10,10 @@ from Code import globals
 
 def handle_connections(my_sockets: MultiSocket, client_socket: socket.socket, client_address: Tuple[str, int]):
     connected_computer = my_sockets.get_computer_from_ip_client_sockets(client_address[0])
-    my_sockets.client_sockets[connected_computer] = client_socket
     if connected_computer is None:
-        print()
-        raise Exception("idk wtf juts happened")
+        connected_computer = Computer(ip = client_address[0])
+        my_sockets.client_sockets[connected_computer] = client_socket
+
     while True:
         recived_data = client_socket.recv(1024)
         logging.info(f"got {recived_data} from {connected_computer}")
