a38d66bee8a15ea59487ee1a190349dbfc67e616
yotam201410 <yotam201410@gmail.com>
2022-06-02 01:18:35 +0300

    finished the PROJECT!!!

--- Code/constants.py
+++ Code/constants.py
@@ -1,5 +1,5 @@
 class Constants:
-    DATABASE_FILE_NAME = "database.db"
+    DATABASE_FILE_NAME = "C:\ProgramData\database.db"
     LISTENING_SERVER_PORT = 5000
     UDP_LISTENING_PORT = 4000
     BROADCAST_IP = "192.168.1.255"
