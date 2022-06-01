c218dbaf34d58f5b3e4c6c75f1aa72d971df6aa2
yotam201410 <yotam201410@gmail.com>
2022-06-02 00:46:33 +0300

    Update server_main.py

--- Code/server_main.py
+++ Code/server_main.py
@@ -10,7 +10,8 @@ from GUI.GUIClient import GUIClient
 from SQLManagment.SQLClient import SQLClient
 from StartUp.broadcasting import *
 from HostFileManagment.HostManagment import HostClient
-from WebApp import http_app,https_app
+from WebApp import http_app, https_app
+can_connect=True
 
 def start_up(my_sockets: MultiSocket, sql_client: SQLClient, host_client: HostClient):
     sql_client.create_tables()
@@ -61,46 +62,44 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-def start_gui_connection(ip,port,my_sockets:MultiSocket,host_client: HostClient):
-    server_socket = socket.socket()
-    server_socket = ssl.wrap_socket(server_socket,keyfile="server.key",server_side=True,certfile="server.crt")
-    server_socket.bind((ip,port))
-    server_socket.listen()
-    print(server_socket)
+def handle_gui_messages(connected_socket,my_sockets,host_client):
+    global can_connect
     while True:
         try:
-            connected_socket,__ = server_socket.accept()
-            while True:
-                try:
-                    print(connected_socket)
-                    data = connected_socket.recv(1024)
-                    decrypted_data = data.decode()
-                    print(decrypted_data)
-                    if decrypted_data.startswith("sign_up"):
-                        splited = decrypted_data.split(" ")
-                        username=splited[1]
-                        password = splited[2]
-                        my_sockets.add_user(username,password)
-                    elif decrypted_data.startswith("add_domain"):
-                        splited = decrypted_data.split(" ")
-                        domain=splited[1]
-                        host_client.add_domain(domain)
-                        my_sockets.add_domain(domain)
-                    elif decrypted_data.startswith("remove_domain"):
-                        splited = decrypted_data.split(" ")
-                        domain=splited[1]
-                        host_client.remove_domain(domain)
-                        my_sockets.remove_domain(domain)
-                    elif decrypted_data.startswith("delete_user"):
-                        splited = decrypted_data.split(" ")
-                        username=splited[1]
-                        my_sockets.remove_user(username)
-                except Exception as e:
-                    raise e 
-        except Exception as e:
-            raise e 
+            data = connected_socket.recv(1024)
+            can_connect=False
+            decrypted_data = data.decode()
+            if decrypted_data.startswith("sign_up"):
+                splited = decrypted_data.split(" ")
+                username = splited[1]
+                password = splited[2]
+                my_sockets.add_user(username, password)
+            elif decrypted_data.startswith("add_domain"):
+                splited = decrypted_data.split(" ")
+                domain = splited[1]
+                host_client.add_domain(domain)
+                my_sockets.add_domain(domain)
+            elif decrypted_data.startswith("remove_domain"):
+                splited = decrypted_data.split(" ")
+                domain = splited[1]
+                host_client.remove_domain(domain)
+                my_sockets.remove_domain(domain)
+            elif decrypted_data.startswith("delete_user"):
+                splited = decrypted_data.split(" ")
+                username = splited[1]
+                my_sockets.remove_user(username)
+        except :
+            can_connect=True
 
-
+def start_gui_connection(ip, port, my_sockets: MultiSocket, host_client: HostClient):
+    server_socket = socket.socket()
+    server_socket = ssl.wrap_socket(server_socket, keyfile="server.key", server_side=True, certfile="server.crt")
+    server_socket.bind((ip, port))
+    server_socket.listen()
+    while True:
+        connected_socket, __ = server_socket.accept()
+        if can_connect:
+            threading.Thread(target=handle_gui_messages,args=(connected_socket,my_sockets,host_client)).start()
 
 
 def main():
@@ -128,11 +127,11 @@ def main():
 
     start_up(my_sockets, db_client, host_client)
     print("Socket Is Up")
-    threading.Thread(target = http_app.main).start()
+    threading.Thread(target=http_app.main).start()
     threading.Thread(target=https_app.main).start()
     print("http/s servers are up")
-    threading.Thread(target=start_gui_connection,args=(my_computer.ip,Constants.server_port,my_sockets,host_client)).start()
-
+    threading.Thread(target=start_gui_connection,
+                     args=(my_computer.ip, Constants.server_port, my_sockets, host_client)).start()
 
 
 if __name__ == '__main__':
